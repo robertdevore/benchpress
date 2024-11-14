9be50870d00d0b423da58dd46b42ce0058aa4846
Robert DeVore <deviodigital@gmail.com>
2024-11-14 01:31:11 -0500

    :bug: BUG: Fixed assets loading on pages outside of BenchPress specific pages

--- benchpress.php
+++ benchpress.php
@@ -354,7 +354,16 @@ function benchpress_render_settings_page() {
  * @since  1.0.0
  * @return void
  */
-function benchpress_enqueue_assets() {
+function benchpress_enqueue_assets( $hook ) {
+    // Bail early if we're not on BenchPress pages.
+    if ( ! in_array( $hook, [
+        'toplevel_page_benchpress',
+        'benchpress_page_benchpress-snapshots',
+        'benchpress_page_benchpress-settings'
+    ], true ) ) {
+        return;
+    }
+
     wp_enqueue_style( 'benchpress-styles', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', [], BENCHPRESS_VERSION );
     wp_enqueue_style( 'select2-css', plugin_dir_url( __FILE__ ) . 'assets/css/select2.min.css', [], BENCHPRESS_VERSION );
     wp_enqueue_script( 'select2-js', plugin_dir_url( __FILE__ ) . 'assets/js/select2.min.js', [ 'jquery' ], BENCHPRESS_VERSION, true );
